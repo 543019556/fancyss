e59d734c3c6abc7569c093ef49e89af85bf2abe3
hq450 <qhuang450@gmail.com>
2018-08-28 14:46:19 +0800

    update 4.04 arm

--- fancyss_arm/config.json.js
+++ fancyss_arm/config.json.js
@@ -1,8 +1,8 @@
 {
 "version":"4.0.3",
-"md5":"b6bcdb0098cf6f1900b28c821d0f1f78",
+"md5":"704bf5077a5481e9f45d6a4b28ea4e52",
 "home_url":"Main_Ss_Content.asp",
 "title":"科学上网",
 "description":"科学上网",
-"build_date":"2018-08-27_19:45:00"
+"build_date":"2018-08-28_14:45:49"
 }
